3257e333ae50b32bcd205ab9d43d96d6a17a2f3e
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-07-11 05:12:03 +0100

    Refactor generateRandomPuzzle to generatePuzzle
    Refactored puzzle generation to use a pool of randomly selected items.

--- engine/generator.js
+++ engine/generator.js
@@ -1,12 +1,23 @@
+function pickFour(arr) {
+    return arr.sort(() => Math.random() - 0.5).slice(0, 4);
+}
+
 function randomItem(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function generateRandomPuzzle() {
-    return {
-        killer: randomItem(suspects).id,
-        weapon: randomItem(weapons).id,
-        location: randomItem(locations).id,
-        motive: randomItem(motives).id
+function generatePuzzle() {
+    const pool = {
+        suspects: pickFour(suspects),
+        weapons: pickFour(weapons),
+        locations: pickFour(locations),
+        motives: pickFour(motives)
     };
+
+    const killer = randomItem(pool.suspects);
+    const weapon = randomItem(pool.weapons);
+    const location = randomItem(pool.locations);
+    const motive = randomItem(pool.motives);
+
+    return { pool, killer, weapon, location, motive };
 }
