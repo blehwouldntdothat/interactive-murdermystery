0946bea601f5697eeb2c85bdec0fb92c02ebd78a
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-07-11 04:55:51 +0100

    Add locations data with names and IDs

---
+++ data/locations.js
@@ -0,0 +1,6 @@
+const locations = [
+    { name: "Kitchen 🍳", id: "kitchen" },
+    { name: "Library 📚", id: "library" },
+    { name: "Garden 🌿", id: "garden" },
+    { name: "Basement 🕳️", id: "basement" }
+];
