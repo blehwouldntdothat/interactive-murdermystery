99b14c2e8e41387ba654d140609dcb72869414ee
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-07-11 05:13:09 +0100

    Refactor presetPuzzles to presetPuzzle structure

--- puzzles/presetPuzzles.js
+++ puzzles/presetPuzzles.js
@@ -1,8 +1,12 @@
-const presetPuzzles = [
-    {
-        killer: "clara",
-        weapon: "poison",
-        location: "library",
-        motive: "revenge"
-    }
-];
+const presetPuzzle = {
+    pool: {
+        suspects: suspects.slice(0, 4),
+        weapons: weapons.slice(0, 4),
+        locations: locations.slice(0, 4),
+        motives: motives.slice(0, 4)
+    },
+    killer: suspects[2],     // Clara
+    weapon: weapons[1],      // Rope
+    location: locations[0],  // Kitchen
+    motive: motives[3]       // Fear
+};
