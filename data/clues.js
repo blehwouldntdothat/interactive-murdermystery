238e62734bbb8400bd8bcd9d081c3a38594400e6
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-07-11 05:26:01 +0100

    Add buildClues function for generating clues
    Refactor clue generation by implementing buildClues function that constructs clues based on puzzle data.

--- data/clues.js
+++ data/clues.js
@@ -1,7 +1,22 @@
-const clueTemplates = [
-    (s, l) => `${s.name} was confirmed to NOT be in the ${l.name}.`,
-    (s, w) => `Traces of ${w.material} were found on ${s.name}'s clothing.`,
-    (w) => `The murder weapon was made of ${w.material}.`,
-    (m) => `The killer’s motive was NOT ${m.name}.`,
-    (l) => `The murder happened in the ${l.name}.`
-];
+function buildClues(puzzle) {
+    const { killer, weapon, location, motive, pool } = puzzle;
+    const clues = [];
+
+    // Murder location
+    clues.push(`The murder happened in the ${location.name}.`);
+
+    // Weapon material
+    clues.push(`The murder weapon was made of ${weapon.material}.`);
+
+    // Eliminate one innocent suspect
+    const innocent = pool.suspects.filter(s => s.id !== killer.id);
+    const eliminated = innocent[Math.floor(Math.random() * innocent.length)];
+    clues.push(`${eliminated.name} was confirmed to NOT be in the ${location.name}.`);
+
+    // Eliminate one wrong motive
+    const wrongMotives = pool.motives.filter(m => m.id !== motive.id);
+    const wrong = wrongMotives[Math.floor(Math.random() * wrongMotives.length)];
+    clues.push(`The killer’s motive was NOT ${wrong.name}.`);
+
+    return clues;
+}
