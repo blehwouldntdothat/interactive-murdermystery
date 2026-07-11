27c7ab4e3d7d36abef294a39dbed2c747dd49508
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-07-11 04:55:39 +0100

    Create weapons.js

---
+++ data/weapons.js
@@ -0,0 +1,6 @@
+const weapons = [
+    { name: "Steel Knife 🔪", material: "steel", id: "knife" },
+    { name: "Poison Vial ☠️", material: "chemical", id: "poison" },
+    { name: "Rope 🧵", material: "fiber", id: "rope" },
+    { name: "Wrench 🔧", material: "metal", id: "wrench" }
+];
